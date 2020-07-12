29dac2b8c32149d32499d2cf39dbe1a18af5df78
DrunkenElf <dolbanutiy@gmail.com>
2020-07-12 18:36:50 +0300

    data classes moved

--- app/src/main/java/com/ui/innoguestapplication/backend/Backend.kt
+++ app/src/main/java/com/ui/innoguestapplication/backend/Backend.kt
@@ -1,7 +1,12 @@
 package com.ui.innoguestapplication.backend
 
-import android.util.JsonToken
 import android.util.Log
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+import com.ui.innoguestapplication.events.Event
+import com.ui.innoguestapplication.events.FaqElem
+import com.ui.innoguestapplication.events.MainEvent
 import com.ui.innoguestapplication.sqlite_database.LoginData
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -11,11 +16,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 import kotlin.NullPointerException
 
-fun validData(resp: ResponseRest): APIRequests.DataState{
+
+fun validData(resp: ResponseRest): APIRequests.DataState {
     return try {
         when(resp.error_request){
             1 -> APIRequests.DataState.NO_TOKEN
-            else -> when(resp.body!!.error){
+            else -> when(resp.body.error){
                 1 -> APIRequests.DataState.WRONG_TOKEN
                 2 -> APIRequests.DataState.USER_NOT_EXIST
                 else -> APIRequests.DataState.NO_ERROR
@@ -26,20 +32,22 @@ fun validData(resp: ResponseRest): APIRequests.DataState{
     }
 }
 
-fun validAuth(resp: ResponseRest): APIRequests.LoginState{
-    return try {
+fun validAuth(resp: ResponseRest): APIRequests.LoginState {
+    try {
         when (resp.error_request) {
-            1 -> APIRequests.LoginState.ERROR
-            else -> when (resp.body!!.success) {
+            1 -> return APIRequests.LoginState.ERROR
+            else -> when (resp.body.success) {
                 0 -> when (resp.body.error) {
-                    1 -> APIRequests.LoginState.WRONG_LOGIN
-                    else -> APIRequests.LoginState.WRONG_PASSWORD
+                    1 -> return APIRequests.LoginState.WRONG_LOGIN
+                    else -> return APIRequests.LoginState.WRONG_PASSWORD
                 }
-                else -> APIRequests.LoginState.NO_ERRORS
+                else -> return APIRequests.LoginState.NO_ERRORS
             }
         }
     } catch (e: NullPointerException){
-        APIRequests.LoginState.ERROR
+        e.printStackTrace()
+        Log.d("validAuth","npe")
+        return APIRequests.LoginState.ERROR
     }
 }
 
@@ -52,14 +60,11 @@ object Backend {
     private class RestAPIService() {
         val retrofit = RestBuilder.buildService(API::class.java)
 
-        fun auth(request: LoginData, callback: Callback<ResponseRest>) {
-            //retrofit = RestBuilder.buildService(API::class.java)
+        fun auth(request: LoginData, callback: Callback<ResponseRest>) =
             retrofit.auth(request.email, request.password).enqueue(callback)
-        }
 
-        fun getData(token: String, callback: Callback<ResponseRest>) {
+        fun getData(token: String, callback: Callback<ResponseRest>) =
             retrofit.getData(token).enqueue(callback)
-        }
     }
 
     private object RestBuilder {
@@ -81,7 +86,37 @@ object Backend {
 
         }
     }
+
+
+
+
 }
+@Keep
+data class ResponseRest(
+        @SerializedName("error_request") val error_request: Int,
+        @SerializedName("body") val body: RespBody
+)
+@Keep
+data class RespBody(
+        @SerializedName("success") val success: Int?,
+        @SerializedName("error") val error: Int?,
+        @SerializedName("data") val data: RespData?
+)
+@Keep
+data class RespData(
+        @SerializedName("token") val token: String,
+        @SerializedName("user") val user: RespUser,
+        @SerializedName("event") val event: MainEvent,
+        @SerializedName("schedule") val schedule: ArrayList<Event?>,
+        @SerializedName("faq") val faq: ArrayList<FaqElem?>
+)
+@Keep
+data class RespUser(
+        @SerializedName("id") val id: Int,
+        @SerializedName("email") val email: String,
+        @SerializedName("name") val name: String,
+        @SerializedName("barcode") val barcode: String
+)
 
 
 
--- app/src/main/java/com/ui/innoguestapplication/backend/RestAPI.kt
+++ app/src/main/java/com/ui/innoguestapplication/backend/RestAPI.kt
@@ -20,30 +20,3 @@ interface API {
     fun getData(@Field("token") token: String): Call<ResponseRest>
 }
 
-
-data class ResponseRest(
-        @SerializedName("error_request") val error_request: Int?,
-        @SerializedName("body") val body: RespBody?
-)
-
-data class RespBody(
-        @SerializedName("success") val success: Int?,
-        @SerializedName("error") val error: Int?,
-        @SerializedName("data") val data: RespData?
-)
-
-data class RespData(
-        @SerializedName("token") val token: String?,
-        @SerializedName("user") val user: RespUser?,
-        @SerializedName("event") val event: MainEvent?,
-        @SerializedName("schedule") val schedule: ArrayList<Event?>?,
-        @SerializedName("faq") val faq: ArrayList<FaqElem?>?
-)
-
-data class RespUser(
-        @SerializedName("id") val id: Int?,
-        @SerializedName("email") val email: String?,
-        @SerializedName("name") val name: String?,
-        @SerializedName("barcode") val barcode: String?
-)
-
